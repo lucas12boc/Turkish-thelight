5210b92877a0db5feaaa86256b68fdbd9c76a0a5
lucas12boc <prosperiapro2025@gmail.com>
2025-08-04 21:54:01 -0300

    next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,34 @@
+import type {NextConfig} from 'next';
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    // This is required to allow requests from the development environment.
+    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+  },
+};
+
+export default withPWA(nextConfig);
