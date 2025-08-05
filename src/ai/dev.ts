37c6653b89cbc21954f0aac06fd3f09f6419b87e
lucas12boc <prosperiapro2025@gmail.com>
2025-08-04 22:12:41 -0300

    dev.ts

---
+++ src/ai/dev.ts
@@ -0,0 +1,5 @@
+// Flows will be imported for their side effects in this file.
+import './flows/fortune-teller-flow';
+import './flows/currency-converter-flow';
+import './flows/dream-interpreter-flow';
+import './flows/slang-of-the-day-flow';
